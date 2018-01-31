e0e24f1cf75d81cd534f5cd4a3a389891498ac0f
Jefferson Puchalski <puchiruzuki@gmail.com>
2018-01-31 15:59:25 -0200

    Fix find files class. Added some stuffs.

--- make_dev_enviroment.py
+++ make_dev_enviroment.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sys, os , threading
 import subprocess
 from time import sleep
 
-
+"""
+Class helper for colored terminal 
+"""
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -13,14 +17,31 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-""" Find programs and add to list """
+    @staticmethod
+    def printout(string, level):
+
+       if level == 'Warning':
+           print(bcolors.WARNING + string + bcolors.ENDC)
+    
+       if level == 'Error':
+           print(bcolors.FAIL + string  + bcolors.ENDC)
+
+       if level == 'OK':
+	   print(bcolors.OKGREEN + string + bcolors.ENDC)
+
+
+""" 
+Class to find programs and add to list 
+"""
 class Find():
 
     dict_result = []
 
     def __init__(self):
-       pass
-    
+        pass
+#----------------------------------------------------------------------
+# Find all installed prograns in system.
+#----------------------------------------------------------------------    
     def findFile(self, name):
        if isinstance(name, list):
            for x in name:
@@ -36,35 +57,49 @@ class Find():
                self.dict_result.append(name)
 
            else:
-               print(bcolors.OKGREEN + "Program %s is found!" + bcolors.ENDC)
+               print(bcolors.OKGREEN + 'Program '+ name + ' is found!' + bcolors.ENDC)
 
+#----------------------------------------------------------------------
+# Get files for instalation and install on computuer
+# --------------------------------------------------------------------
     def installFiles(self):
         if isinstance(self.dict_result, list):
             for x in self.dict_result:
-		if x == 'visualcode':
-			reponse = subprocess.call (['wget','https://go.microsoft.com/fwlink/?LinkID=760868', '-O', 'vscode.deb'])
-			if response == 0:
-			    subprocess.call(['dpkg', '-i', 'vscode.deb'])
-			    subprocess.call(['apt', 'install', '-f']) 
-		if x == 'dbeaver':
-#                        subprocess.call (['wget','https://go.microsoft.com/fwlink/?LinkID=760868'])
+		    if x == 'vscode':
+		        if os.path.exists('vscode.deb'):
+		            bcolors.printout("Arquivo vscode.dev já existe, abortando download", 'Warning')
+			        subprocess.call(['dpkg', '-i', 'vscode.deb'])
+                    subprocess.call(['apt', 'install', '-f']) 
+		        else:	
+		            response = subprocess.call (['wget','https://az764295.vo.msecnd.net/stable/7c4205b5c6e52a53b81c69d2b2dc8a627abaa0ba/code_1.19.3-1516876437_amd64.deb', '-O', 'vscode.deb'])
+		            if response == 0:
+		                subprocess.call(['dpkg', '-i', 'vscode.deb'])
+			            subprocess.call(['apt', 'install', '-f'])
 
-	        result = subprocess.call(['apt','install', x , '-y'])
-                if result != 0:
-                   print(bcolors.FAIL + "the program " + x + " cant be installed by apt. Need manual installation" + bcolors.ENDC)
-                else:
-                   print(bcolors.OKGREEN + "Program " + x + " is installed!" + bcolors.ENDC)
-		   
-	else:
-	   print(bcolors.OKGREEN + "Program " + x + "is installed!" + bcolors.ENDC)
-
-
+	        if x == 'dbeaver':
+		        if os.path.exists('dbeaver.deb'):
+		            bcolors.printout('Arquivo dbeaver.deb já existe, abortando download', 'Warning')
+			        subprocess.call(['dpkg', '-i', 'dbeaver.deb'])
+			        subprocess.call(['apt', 'install', '-f'])
+		        else:
+	                response = subprocess.call (['wget','https://dbeaver.jkiss.org/files/dbeaver-ce_latest_amd64.deb', '-O', 'dbeaver.deb'])
+		            if response == 0:
+		                subprocess.call(['dpkg', '-i', 'dbeaver.deb'])
+			            subprocess.call(['apt', 'install', '-f'])
+		
+		#Install all procress pendending.
+	    result = subprocess.call(['apt','install', x , '-y'])
+                
+		if result != 0:
+            bcolors.printout('The program ' + x + ' can\'t be installed by apt. Need manual installation','Error')
+        else:
+            bcolors.printout('Program ' + x + ' is installed!', 'OK')
 
 
 if __name__ == "__main__":
-    print ("Initalizing database...")
+    bcolors.printout('Initalizing database...', 'OK')
     f = Find()
-    program = ["dbeaver", "docker", "visualcode", "openjdk-8-jre", "eclipse"]
+    program = ["git", "dbeaver", "docker", "vscode", "openjdk-8-jre", "eclipse"]
     f.findFile(program)
     f.installFiles()
 
